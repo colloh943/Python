551fc4952b8161e0bd50bbd7e1f2f880a765e4b8
colloh943 <collinsnyamboga53@gmail.com>
2025-02-12 14:47:49 +0300

    Update try.py

--- try.py
+++ try.py
@@ -12,23 +12,23 @@ else:
     print(number)
 
 
-# number= input("Enter a number: ")
-# if (number/3) and (number/5):
-#     print("FuzzBuzz")
-# elif (num/3):
-#     print("Fuzz")
-# elif (num/5):
-#     print("Buzz")
-# else:
-#     print(num)
+number= input("Enter a number: ")
+if (number/3) and (number/5):
+    print("FuzzBuzz")
+elif (num/3):
+    print("Fuzz")
+elif (num/5):
+    print("Buzz")
+else:
+    print(num)
 
 
-# withdraw_amount=input("Enter your withdrawal amount: ")
-# if withdraw_amount <=100 :
-#     print("charges =0")
-# else:
-#     charges=withdraw_amount+10/100
-#     print("charges =",charges)
+withdraw_amount=input("Enter your withdrawal amount: ")
+if withdraw_amount <=100 :
+    print("charges =0")
+else:
+    charges=withdraw_amount+10/100
+    print("charges =",charges)
 
 
 
