7b396f6c9e9ea307ecb9c26fac193ea06f818bcc
colloh943 <collinsnyamboga53@gmail.com>
2025-02-12 14:45:29 +0300

    second coommit

--- conditional statements.py
+++ conditional statements.py
@@ -1,87 +1,87 @@
-# if....else...
-# votes = 500
-# if votes >=10000:
-#     print(f"you got {votes} and therefor you win!")
-# else:
-#     print(f"you got {votes} and therefor you lose")
-#
-#
-#
-#
-# age=float(input("enter your age"))
-# if age>18:
-#     print("you are eligible to vote")
-# else:
-#     print("you are not eligible to vote")
-#
-# number=float(input("enter your number"))
-# if number > 0:
-#         print('number positive')
-# elif number < 0:
-#         print("number negative")
-# else:
-#     print("0")
-#
-#
-#
-#
-# marks=70
-# if marks >= 80 and marks <= 100:
-#     print(f"you got {marks} and you have grade A ")
-# elif marks >= 70 and marks <= 89:
-#     print(f"you got {marks} and you have grade B ")
-# elif marks >= 60 and marks <= 69:
-#     print(f"you got {marks} and you have grade C ")
-# elif marks >=40 and marks <= 59:
-#     print(f"you got {marks} and you have grade D ")
-# elif marks <= 40 and marks >= 0:
-#     print(f"you got {marks} and you have grade E ")
-# else:
-#     print("please enter a number between 0 and 100 ")
-#
-#
-#
-#
-#
-#
-# temp = float(input("Enter the temperature in degrees Celsius: "))
-# if temp > 30:
-#     print("It is a hot day ")
-# elif temp < 30 and temp > 20:
-#     print("It is a warm day ")
-# else:
-#     print("It is a cold day ")
-#
-#
-#
-#
-#
-#
-# age=float(input("enter your age"))
-# if age >2:
-#     print("You are a baby")
-# elif age>=4 and age<= 12:
-#     print("you are a child")
-# elif age>=13 and age<=19:
-#         print("you are a teenager")
-# elif age>=20 and age<=34:
-#         print("you are a young adult")
-# elif age>=35 and age<=59:
-#     print("you are a senior citizen")
-# else:
-#     print("you are old")
-#
-#
-# print("age is just a number enjoy life")
-#
-#
-#
-#
-#
-#
-#
-#
-#
+if....else...
+votes = 500
+if votes >=10000:
+    print(f"you got {votes} and therefor you win!")
+else:
+    print(f"you got {votes} and therefor you lose")
+
+
+
+
+age=float(input("enter your age"))
+if age>18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+number=float(input("enter your number"))
+if number > 0:
+        print('number positive')
+elif number < 0:
+        print("number negative")
+else:
+    print("0")
+
+
+
+
+marks=70
+if marks >= 80 and marks <= 100:
+    print(f"you got {marks} and you have grade A ")
+elif marks >= 70 and marks <= 89:
+    print(f"you got {marks} and you have grade B ")
+elif marks >= 60 and marks <= 69:
+    print(f"you got {marks} and you have grade C ")
+elif marks >=40 and marks <= 59:
+    print(f"you got {marks} and you have grade D ")
+elif marks <= 40 and marks >= 0:
+    print(f"you got {marks} and you have grade E ")
+else:
+    print("please enter a number between 0 and 100 ")
+
+
+
+
+
+
+temp = float(input("Enter the temperature in degrees Celsius: "))
+if temp > 30:
+    print("It is a hot day ")
+elif temp < 30 and temp > 20:
+    print("It is a warm day ")
+else:
+    print("It is a cold day ")
+
+
+
+
+
+
+age=float(input("enter your age"))
+if age >2:
+    print("You are a baby")
+elif age>=4 and age<= 12:
+    print("you are a child")
+elif age>=13 and age<=19:
+        print("you are a teenager")
+elif age>=20 and age<=34:
+        print("you are a young adult")
+elif age>=35 and age<=59:
+    print("you are a senior citizen")
+else:
+    print("you are old")
+
+
+print("age is just a number enjoy life")
+
+
+
+
+
+
+
+
+
 
 
 
